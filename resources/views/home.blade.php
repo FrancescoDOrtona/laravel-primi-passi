56cf63bd5e985f273cf71d61e2844a5e9f04c991
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-07 17:29:48 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>{{ $title }}</h1>
+            <nav>
+                <a href="">Home</a>
+                <a href="/about-us">About Us</a>
+            </nav>
+        </header>
+    </div>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolores nisi laborum ad cumque debitis, impedit quae magni molestiae tempore libero culpa. Dolore aut suscipit inventore possimus illo, similique consequuntur.</p>
+    </div>
+</body>
+</html>
